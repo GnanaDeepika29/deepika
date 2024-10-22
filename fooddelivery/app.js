34e4e56a3a07ee5af0ebe3a163762fb127038bba
GnanaDeepika29 <gnanadeepika2982004@gmail.com>
2024-10-22 11:29:40 +0530

    Update app.js

--- fooddelivery/app.js
+++ fooddelivery/app.js
@@ -7,16 +7,52 @@ function openFoodModal(restaurantName, deliveryTime, restaurantId) {
     const foodItems = {
         pizza: [
             { name: "Margherita Pizza", price: 10, img: "margherita.jpg" },
-            { name: "Pepperoni Pizza", price: 12, img: "pepperoni.jpg" }
+            { name: "Pepperoni Pizza", price: 12, img: "pepperoni.jpg" },
+            { name: "BBQ Chicken Pizza", price: 14, img: "bbq-chicken.jpg" },
+            { name: "Vegetarian Pizza", price: 11, img: "vegetarian.jpg" },
         ],
         burger: [
             { name: "Cheeseburger", price: 8, img: "cheeseburger.jpg" },
-            { name: "Veggie Burger", price: 7, img: "veggie-burger.jpg" }
+            { name: "Veggie Burger", price: 7, img: "veggie-burger.jpg" },
+            { name: "Bacon Burger", price: 10, img: "bacon-burger.jpg" },
+            { name: "Double Patty Burger", price: 12, img: "double-patty.jpg" },
         ],
         sushi: [
             { name: "California Roll", price: 15, img: "california-roll.jpg" },
-            { name: "Spicy Tuna Roll", price: 18, img: "spicy-tuna-roll.jpg" }
-        ]
+            { name: "Spicy Tuna Roll", price: 18, img: "spicy-tuna-roll.jpg" },
+            { name: "Dragon Roll", price: 20, img: "dragon-roll.jpg" },
+            { name: "Tempura Roll", price: 17, img: "tempura-roll.jpg" },
+        ],
+        taco: [
+            { name: "Beef Taco", price: 5, img: "beef-taco.jpg" },
+            { name: "Chicken Taco", price: 5, img: "chicken-taco.jpg" },
+            { name: "Veggie Taco", price: 4, img: "veggie-taco.jpg" },
+            { name: "Fish Taco", price: 6, img: "fish-taco.jpg" },
+        ],
+        indian: [
+            { name: "Butter Chicken", price: 12, img: "butter-chicken.jpg" },
+            { name: "Paneer Tikka", price: 10, img: "paneer-tikka.jpg" },
+            { name: "Biryani", price: 15, img: "biryani.jpg" },
+            { name: "Dal Makhani", price: 9, img: "dal-makhani.jpg" },
+        ],
+        italian: [
+            { name: "Spaghetti Carbonara", price: 13, img: "carbonara.jpg" },
+            { name: "Fettuccine Alfredo", price: 14, img: "alfredo.jpg" },
+            { name: "Lasagna", price: 15, img: "lasagna.jpg" },
+            { name: "Penne Arrabbiata", price: 12, img: "penne.jpg" },
+        ],
+        thai: [
+            { name: "Pad Thai", price: 11, img: "pad-thai.jpg" },
+            { name: "Green Curry", price: 13, img: "green-curry.jpg" },
+            { name: "Tom Yum Soup", price: 10, img: "tom-yum.jpg" },
+            { name: "Thai Spring Rolls", price: 8, img: "spring-rolls.jpg" },
+        ],
+        dessert: [
+            { name: "Chocolate Cake", price: 6, img: "chocolate-cake.jpg" },
+            { name: "Cheesecake", price: 7, img: "cheesecake.jpg" },
+            { name: "Ice Cream Sundae", price: 5, img: "ice-cream.jpg" },
+            { name: "Fruit Tart", price: 8, img: "fruit-tart.jpg" },
+        ],
     };
 
     // Clear existing items
@@ -69,7 +105,7 @@ function placeOrder() {
     const coupon = document.getElementById("coupon").value;
 
     const orderSummary = document.getElementById("order-summary");
-    const totalPrice = quantity * 10; // Assume $10 for each food item
+    const totalPrice = quantity * 10; // Assume $10 for each food item (you can modify based on selected item)
     orderSummary.innerHTML += `<p>Order placed! Total: $${totalPrice}</p>`;
 
     document.getElementById("order-status").innerText = "Thank you for your order!";
